91fb88f913d5aacd13ddd0e468d2615250bada9b
tat0154 <tylertice@my.unt.edu>
2023-05-11 02:04:34 -0500

    Echo implemented

--- builtins/echo.go
+++ builtins/echo.go
@@ -5,12 +5,16 @@ package builtins
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 var (
-	errInvalidArgs = errors.New("invalid arguments")
-	errMissingArgs = errors.New("missing arguments")
+	errInvalidArgs  = errors.New("invalid arguments")
+	errMissingArgs  = errors.New("missing arguments")
+	enableBackslash bool
+	trailingNewline bool
+	startIndex      int
 )
 
 func Echo(args ...string) error {
@@ -20,31 +24,69 @@ func Echo(args ...string) error {
 		return fmt.Errorf("%w: unexpected arguments", errMissingArgs)
 	}
 
-	//Check for declared options
+	//OUTPUT OPTIONS
+	//Set Defaults
+	enableBackslash = false
+	trailingNewline = true
+
+	//Check if specified options are set
 	hasOptions := strings.HasPrefix(args[0], "-")
 
 	if hasOptions { //Option Declared
 		option := strings.TrimPrefix(args[0], "-")
 
 		switch option {
-		case "n":
-			fmt.Print("-n specified")
 		case "e":
-			fmt.Print("-e specified")
-		case "E":
-			fmt.Print("-E specified")
+			enableBackslash = true
+		case "E": //Default options
+			break
 		case "ne":
-			fmt.Print("-ne specified")
+			trailingNewline = false
+			enableBackslash = true
 		case "nE":
-			fmt.Print("-nE specified")
+			trailingNewline = false
+		case "n":
+			trailingNewline = false
 		default:
 			return fmt.Errorf("%w: unexpected option", errInvalidArgs)
 		}
-
-	} else {
-		return fmt.Errorf("%w: unexpected arguments", errInvalidArgs)
 	}
 
-	return fmt.Errorf("end of func")
+	//PRINT ARGUMENTS
+
+	//Setup and check args
+	if hasOptions {
+		startIndex = 1
+		if len(args) < 2 {
+			return fmt.Errorf("%w: unexpected arguments", errMissingArgs)
+		}
+	} else {
+		startIndex = 0
+		if len(args) < 1 {
+			return fmt.Errorf("%w: unexpected arguments", errMissingArgs)
+		}
+	}
+
+	//Print loop
+	for i := startIndex; i < len(args); i++ {
+		if enableBackslash {
+			modStr, err := strconv.Unquote(`"` + args[i] + `"`)
+			if err != nil {
+				return fmt.Errorf("%w: invalid arguments", errInvalidArgs)
+			}
+			fmt.Print(modStr)
+		} else {
+
+			fmt.Print(args[i])
+		}
+
+		fmt.Print(" ") //Add space between arguments
+	}
+
+	if trailingNewline {
+		fmt.Printf("\n")
+	}
+
+	return nil
 
 }
